5be30b51d10b57ac9d8be26e678f62f32a7252e3
KANHU CHARAN BEHERA <beherakanha029@gmail.com>
2025-04-02 15:16:34 +0530

    Add files via upload

---
+++ 04-Spring-Annotations-App/src/main/java/in/ashokit/config/AppConfig.java
@@ -0,0 +1,21 @@
+package in.ashokit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import in.ashokit.beans.Robot;
+
+@Configuration
+@ComponentScan(basePackages= {"in.ashokit","com.ibm"})
+public class AppConfig {
+	public AppConfig() {
+		System.out.println("AppConfig :: Constructor");
+	}
+	
+	@Bean
+	public Robot buildRobot() {
+		Robot r=new Robot();
+		return r;
+	}
+}
